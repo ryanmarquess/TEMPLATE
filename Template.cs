e15a85d9f6cbe2c545d34c73953241dd44c06a3c
ryanmarquess <118925077+ryanmarquess@users.noreply.github.com>
2023-04-11 15:30:42 -0300

    Update Template.cs

--- Template.cs
+++ Template.cs
@@ -1,16 +1,11 @@
 namespace Template.Models;
 public class histórico
 {
-/* No trecho de código a seguir são criados os 
-atributos de Cliente e seus getters e setters
-correspondentes. */
 public int Data { get; set; }
 public string Versão { get; set; }
 public string Descrição { get; set; }
 public string Autor { get; set; }
-/* Nesse trecho do código, é criado o Construtor do 
-model Cliente e determinado quais parâmetros são 
-necessários na criação das instâncias. */
+
 public histórico(int data, string versão, string descrição, string autor)
 {
 this.Data = data;
